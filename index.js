0d8e346c7f4e70558d54939d92e4aa6bada105c5
Nickolas lira7 <125027693+Nickolaslira7@users.noreply.github.com>
2024-05-06 11:08:30 -0300

    Personagem postado

--- index.js
+++ index.js
@@ -24,6 +24,29 @@ app.get("/hero", async (req, res) => {
     }
 });
 
+app.get("/hero/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+        const { rows } = await pool.query("SELECT * FROM personagem WHERE id = $1", [id]);
+        res.send(rows);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+});
+
+app.post("/hero", async (req, res) => {
+    const { nome, vida, dano, defesa} = req.body;
+    try {
+        await pool.query("INSERT INTO personagem (nome, vida, dano, defesa) VALUES ($1, $2, $3, $4)", [nome, vida, dano, defesa]);
+        res.send("Personagem criado com sucesso!");
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+});
+
+
 app.get("/", (req, res) => {
     res.send("Seja Bem-vindo a API de HEROS");
 });
